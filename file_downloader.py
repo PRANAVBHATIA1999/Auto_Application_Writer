8e9ffa62dae4486010211b653788a020b73428c4
PRANAVBHATIA1999 <pranavbhatia431999@gmail.com>
2018-09-05 18:07:36 +0530

    5th Sept 2018

---
+++ file_downloader.py
@@ -0,0 +1,7 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+def docdownloader():
+    file1 = drive.CreateFile()
+    file1.SetContentFile(C:\Users\hpadmin\PycharmProjects\Application_Writer)
+    file1.Upload()
